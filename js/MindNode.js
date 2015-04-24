32f59bb530158833dea6144210964ee1dfdefbb5
戴政 <843976213@qq.com>
2015-04-25 00:27:27 +0800

    Finish the framework of SCUTMind. * Finish the definition of MindNode type,add some attribute and method. * Add some global function in global object SCUTMind.

--- js/MindNode.js
+++ js/MindNode.js
@@ -1,33 +1,63 @@
 //This file defines some constructor of some object type
 
 //A constructor of MindNode type
-//@@parameter pNode to initialize
 function MindNode(type,pNode,position,theme){
     this.type = type;
     this.parent = pNode;
     this.children = [];
     this.position = position;
     this.theme = theme;
-    this.scope = null;
+    this.scope = SCUTMind.initNodeScope(theme,position,"");
+    this.text = "";
+    this.textColor = "#ffffff";
 }
-
+s
 MindNode.prototype = {
     constructor : MindNode,
-    //Function drawIn is used to draw this node in the canvas.
-    //@@parameter cxt is used to as the canvas context which we want to draw the node in.
-    drawIn : function (cxt) {
-
-    },
-    //Function addChild is used to add a child node of this node.
-    //@@parameter node is used to as the child node.
+    /*
+     @method addChild
+     @param node {MindNode} the child node which you want to add.
+     @return this {MindNode}
+     */
     addChild : function (node) {
         this.children.push(node);
         return this;
     },
-    //Function delete is used to delete a child node of this node.
-    //@@parameter node is used to as the child node.
+    /*
+     @method deleteChild
+     @param node {MindNode} the child node which you want to delete.
+     @return this {MindNode}
+     */
     deleteChild : function (node) {
         this.children.splice(this.children.indexOf(node),1);
         return this;
+    },
+    /*
+     @method changeText
+     @param text {string} text you want to change.
+     @return this {MindNode}
+     */
+    changeText : function (text) {
+        this.text = text;
+        this.scope = SCUTMind.initNodeScope(this.type,this.theme,this.position,text);
+        return this;
+    },
+    /*
+     @method changeText
+     @param text {string} text you want to change.
+     @return this {MindNode}
+     */
+    changePosition : function (position) {
+        this.position = position;
+        this.scope = SCUTMind.initNodeScope(this.type,this.theme,position,this.text);
+        return this;
+    },
+    /*
+     @method contains
+     @param position {array} the position you want to judge.
+     @return {bool}
+     */
+    contains : function (position) {
+        return ((position[0] >= this.scope[0]) && (position[0] <= this.scope[2]) && (position[1] >= this.scope[1]) && (position[1] <= this.scope[3]));
     }
 };
--- js/SCUTMindExtension.js
+++ js/SCUTMindExtension.js
@@ -6,30 +6,123 @@ var SCUTMind = {};
 //Global function and variable can be added in the extension Object.
 
 //Global variable
+SCUTMind.backgroundCanvas = [];
+SCUTMind.backgroundCenter = [];
+SCUTMind.currCanvasScope = [];
+SCUTMind.rootNode = null;
+SCUTMind.currNode = null;
+SCUTMind.currPattern = null;
 SCUTMind.themes = {
-    "default" : "default"
+    default : "default"
 //add theme in this object.
 };
-
-//*Function init is to initialize the whole canvas.
-SCUTMind.init = function () {
-
+SCUTMind.pattern = {
+    default : "default"
+//add pattern in this object.
 };
 
-//*Function draws is to draw the nodes.
-//@@parameter cxt to as the node and parent node of the node which we want to draw.
-//@@parameter node to as the canvas context which we want to draw in.
+
+/*
+ @method init
+ @param cxt {Object} the context of web page's canvas.
+ @param theme {string} the theme user choose.
+ @param bgWidth & bgHeight {number} the whole canvas size.
+ @param canvasW & canvasH {number} the size of web page's canvas.
+ @return this {SCUTMind}
+ */
+SCUTMind.init = function (cxt,theme,pattern,bgWidth,bgHeight,canvasW,canvasH) {
+    this.backgroundCanvas = [bgWidth,bgHeight];
+    this.backgroundCenter = [bgWidth/2,bgHeight/2];
+    this.currCanvasScope = [(bgWidth - canvasW)/2,(bgHeight - canvasH)/2,(bgWidth + canvasW)/2,(bgHeight + canvasH)/2];
+    this.currPattern = pattern;
+    if (SCUTMind.rootNode) {
+        this.rootNode = new MindNode("main",null,this.backgroundCenter,theme);
+    }
+    this.draws(cxt,this.rootNode);
+    return this;
+};
+
+/*
+ @method draws
+ @param cxt {Object} the context of web page's canvas.
+ @param node {MindNode} the root node of the tree which you want to draws.
+ @return this {SCUTMind}
+ */
 SCUTMind.draws = function (cxt,node) {
-    node.drawIn(cxt);
+    this.draw(cxt,node);
     for (var i = 0; i < node.children.length; i++) {
         this.draws(cxt, node.children[i]);
     }
-    return true;
+    return this;
 };
 
-//*Function createMainNode is to create the main node.
-//@@parameter position is used to as the position of this node.
-//@@parameter theme is used to as the theme of this node.
-SCUTMind.createMainNode = function (position,theme) {
-    return new MindNode("main",null,position,theme);
+/*
+ @method findNode
+ @param position {array} the position you want to judge.
+ @param node {MindNode} the root node of the tree which you want to judge.
+ @return {MindNode} if there are some node contains the position.
+ @return false if there aren't any node contains the position.
+ */
+SCUTMind.findNode = function (position,node) {
+    var thisChild;
+    if (node.contains(position)){
+        return node;
+    }else{
+        for (var i = 0; i < node.children.length; i++){
+            thisChild = this.findNode(position,node);
+            if(thisChild){
+                return thisChild;
+            }
+        }
+    }
+    return false;
 };
+
+/*
+ @method moveCanvas
+ @param changeX & changeY {number} the X & Y distance which the canvas move.
+ @return this {SCUTMind}
+ */
+SCUTMind.moveCanvas = function (changeX,changeY) {
+    this.currCanvasScope[0] += changeX;
+    this.currCanvasScope[1] += changeX;
+    this.currCanvasScope[2] += changeY;
+    this.currCanvasScope[3] += changeY;
+    return this;
+};
+
+/*
+ @method changeTheme
+ @param theme {string} the new theme.
+ @param node {MindNode} the root node of the tree which you want to change its theme.
+ @return this {SCUTMind}
+ */
+SCUTMind.changeTheme = function (theme,node) {
+    node.theme = theme;
+    for (var i = 0; i < node.children.length; i++) {
+        this.changeTheme(theme, node.children[i]);
+    }
+    return this;
+};
+
+
+
+
+//this is some function about front end achievement
+/*
+ @method draw
+ @param cxt {Object} the context of web page's canvas.
+ @param node {MindNode} the node which you want to draws.
+ @return this {SCUTMind}
+ */
+SCUTMind.draw = function (cxt,node) {};
+
+/*
+ @method initNodeScope
+ @param type {string} the type of the node.
+ @param theme {string} the theme of the node.
+ @param position {array} the position of the node.
+ @param text {string} the text of the node.
+ @return nodeScope {array}
+ */
+SCUTMind.initNodeScope = function (type,theme,position,text) {};
