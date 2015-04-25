f45fa0921703fbdd5d5e0c407ac8d9a9c36b2d27
lazybone <1119357381@qq.com>
2015-04-26 07:57:04 +0800

    Add more themes.

--- js/theme.js
+++ js/theme.js
@@ -3,27 +3,451 @@
 // You can change or add theme in this file.
 
 /*
-@theme_default
+@theme_default also known as theme_gray
 */
 var theme_default = {
 	//shape
-	ancestor_shape : "circle",
-	child_shape : "rectangle",
+	ancestor_shape : "capsule",
+	child_shape : "rounded_rectangle",
 	line_shape : "straight",
 
-	//color
-	background_color : "#000000",
-	element_color : "#66CFF6",
-	text_color : "#000000",
-	line_color : "#ccc",
-	focus_border_color : "#000000",
+    //decoration
+    background_color : "#fff",
+    line_color : "#8e8e8e",
+    focus_border_color : "#000000",
+    common_border_color : "#ccc",
+    focus_border_width : 2,
+    common_border_width : 1,
 
-	//size
-	element_width : 60,
-    element_height : 25,
-	father_child_margin : 30,
-	brother_margin : 20,
+	//ancestor_color
+	anc_element_color : "#ccc",
+	anc_text_color : "#fff",
+
+	//child_color
+	ch_element_color : "#E0DFE0",
+	ch_text_color : "#000",
+
+    //grandchild_color 
+    gra_element_color : "#fff"；
+    gra_text_color : "#000";
+
+    //element_margin
+    father_child_margin : 20,
+	brother_margin : 15,
+
+	//ancestor_size
+	anc_element_width : 80,
+	anc_element_height : 35,
+
+	//child_size
+	ch_element_width : 60,
+	ch_element_height : 27,
 
 	//else
 	background_img : "none"
 };
+
+/*
+@theme_yellow
+*/
+var theme_yellow = {
+	//shape
+	ancestor_shape : "capsule",
+	child_shape : "rounded_rectangle",
+	line_shape : "straight",
+
+    //decoration
+    background_color : "#fff",
+    line_color : "#8e8e8e",
+    focus_border_color : "#C38707",
+    common_border_color : "#fac75b",
+    focus_border_width : 2,
+    common_border_width : 1,
+
+	//ancestor_color
+	anc_element_color : "#fac75b",
+	anc_text_color : "fff",
+
+	//child_color
+	ch_element_color : "#FEE4C2",
+	ch_text_color : "#000",
+
+    //grandchild_color 
+    gra_element_color : "#fff"；
+    gra_text_color : "#000";
+
+	//element_margin
+    father_child_margin : 20,
+	brother_margin : 15,
+
+	//ancestor_size
+	anc_element_width : 80,
+	anc_element_height : 35,
+
+	//child_size
+	ch_element_width : 60,
+	ch_element_height : 27,
+
+	//else
+	background_img : "none"
+};
+
+/*
+@theme_green
+*/
+var theme_green = {
+	//shape
+	ancestor_shape : "capsule",
+	child_shape : "rounded_rectangle",
+	line_shape : "straight",
+
+    //decoration
+    background_color : "#fff",
+    line_color : "#8e8e8e",
+    focus_border_color : "#09816A",
+    common_border_color : "#00c5ad",
+    focus_border_width : 2,
+    common_border_width : 1,
+
+	//ancestor_color
+	anc_element_color : "#00c5ad",
+	anc_text_color : "fff",
+
+    //child_color
+	ch_element_color : "#A6FBD8",
+	ch_text_color : "#000",
+
+    //grandchild_color 
+    gra_element_color : "#fff"；
+    gra_text_color : "#000";
+
+	//element_margin
+    father_child_margin : 20,
+	brother_margin : 15,
+
+	//ancestor_size
+	anc_element_width : 80,
+	anc_element_height : 35,
+
+	//child_size
+	ch_element_width : 60,
+	ch_element_height : 27,
+
+	//else
+	background_img : "none"
+};
+
+/*
+@theme_blue
+*/
+var theme_blue = {
+	//shape
+	ancestor_shape : "capsule",
+	child_shape : "rounded_rectangle",
+	line_shape : "straight",
+
+    //decoration
+    background_color : "#fff",
+    line_color : "#8e8e8e",
+    focus_border_color : "#18376A",
+    common_border_color : "#3bbce0",
+    focus_border_width : 2,
+    common_border_width : 1,
+
+	//ancestor_color
+	anc_element_color : "#3bbce0",
+	anc_text_color : "fff",
+
+    //child_color
+	ch_element_color : "#B3E7FF",
+	ch_text_color : "#000",
+
+    //grandchild_color 
+    gra_element_color : "#fff"；
+    gra_text_color : "#000";
+
+	//element_margin
+    father_child_margin : 20,
+	brother_margin : 15,
+
+	//ancestor_size
+	anc_element_width : 80,
+	anc_element_height : 35,
+
+	//child_size
+	ch_element_width : 60,
+	ch_element_height : 27,
+
+	//else
+	background_img : "none"
+};
+
+/*
+@theme_pink
+*/
+var theme_pink = {
+	//shape
+	ancestor_shape : "capsule",
+	child_shape : "rounded_rectangle",
+	line_shape : "straight",
+
+    //decoration
+    background_color : "#fff",
+    line_color : "#8e8e8e",
+    focus_border_color : "#994B69",
+    common_border_color : "#e75d66",
+    focus_border_width : 2,
+    common_border_width : 1,
+
+	//ancestor_color
+	anc_element_color : "#e75d66",
+	anc_text_color : "fff",
+
+    //child_color
+	ch_element_color : "#FDDFDF",
+	ch_text_color : "#000",
+
+    //grandchild_color 
+    gra_element_color : "#fff"；
+    gra_text_color : "#000";
+
+	//element_margin
+    father_child_margin : 20,
+	brother_margin : 15,
+
+	//ancestor_size
+	anc_element_width : 80,
+	anc_element_height : 35,
+
+	//child_size
+	ch_element_width : 60,
+	ch_element_height : 27,
+
+	//else
+	background_img : "none"
+};
+
+/*
+@theme_purple
+*/
+var theme_purple = {
+	//shape
+	ancestor_shape : "capsule",
+	child_shape : "rounded_rectangle",
+	line_shape : "straight",
+
+    //decoration
+    background_color : "#fff",
+    line_color : "#8e8e8e",
+    focus_border_color : "#39105E",
+    common_border_color : "#9222dd",
+    focus_border_width : 2,
+    common_border_width : 1,
+
+	//ancestor_color
+	anc_element_color : "#9222dd",
+	anc_text_color : "fff",
+
+    //child_color
+	ch_element_color : "#CAADDC",
+	ch_text_color : "#000",
+
+    //grandchild_color 
+    gra_element_color : "#fff"；
+    gra_text_color : "#000";
+
+	//element_margin
+    father_child_margin : 20,
+	brother_margin : 15,
+
+	//ancestor_size
+	anc_element_width : 80,
+	anc_element_height : 35,
+
+	//child_size
+	ch_element_width : 60,
+	ch_element_height : 27,
+
+	//else
+	background_img : "none"
+};
+
+/*
+@theme_classic SCUT mind
+*/
+var theme_classic = {
+	//shape
+	ancestor_shape : "capsule",
+	child_shape : "rounded_rectangle",
+	line_shape : "straight",
+
+    //decoration
+    background_color : "#6f916f",
+    line_color : "#fff",
+    focus_border : "none",
+    common_border : "none",
+    focus_element_color : "#8CF56A",
+
+	//ancestor_color
+	anc_element_color : "#667b99",
+	anc_text_color : "#D4C070",
+
+    //child_color
+	ch_element_color : "#C37373",
+	ch_text_color : "#fff",
+
+
+    //grandchild_color 
+    gra_element_color : "#6f916f"；
+    gra_text_color : "#000";
+
+	//element_margin
+    father_child_margin : 20,
+	brother_margin : 15,
+
+	//ancestor_size
+	anc_element_width : 80,
+	anc_element_height : 35,
+
+	//child_size
+	ch_element_width : 60,
+	ch_element_height : 27,
+
+	//else
+	background_img : "none"
+};
+
+/*
+@theme_tiny SCUT mind
+*/
+var theme_tiny = {
+	//shape
+	ancestor_shape : "rounded_rectangle",
+	child_shape : "capsule",
+	line_shape : "straight",
+
+    //decoration
+    background_color : "#344370",
+    line_color : "#fff",
+    focus_border : "none",
+    common_border : "none",
+    focus_element_color : "#6BC6EB",
+
+	//ancestor_color
+	anc_element_color : "#C34AFA",
+	anc_text_color : "#fff",
+
+    //child_color
+	ch_element_color : "#667b99",
+	ch_text_color : "#e75d66",
+
+    //grandchild_color 
+    gra_element_color : "#344370"；
+    gra_text_color : "#000";
+
+	//element_margin
+    father_child_margin : 14,
+	brother_margin : 10,
+
+	//ancestor_size
+	anc_element_width : 55,
+	anc_element_height : 25,
+
+	//child_size
+	ch_element_width : 40,
+	ch_element_height : 22,
+
+	//grandchild
+	common_border ："hidden";
+    
+	//else
+	background_img : "none"
+};
+
+/*
+@theme_circle
+*/
+var theme_circle = {
+	//shape
+	ancestor_shape : "circle",
+	child_shape : "capsule",
+	line_shape : "straight",
+
+    //decoration
+    background_color : "#4CDBF4",
+    line_color : "#667b99",
+    focus_border : "none",
+    common_border : "none",
+    focus_element_color : "#5B1156",
+
+	//ancestor_color
+	anc_element_color : "#e8e88e",
+	anc_text_color : "#D4C070",
+
+    //child_color
+	ch_element_color : "#88F6AE",
+	ch_text_color : "#000",
+
+    //grandchild_color 
+    gra_element_color : "#4cdbf4"；
+    gra_text_color : "#000";
+
+	//element_margin
+    father_child_margin : 20,
+	brother_margin : 15,
+
+	//ancestor_size
+	anc_element_width : 80,
+	anc_element_height : 35,
+
+	//child_size
+	ch_element_width : 60,
+	ch_element_height : 27,
+
+	//else
+	background_img : "none"
+};
+
+/*
+@theme_soft with all rounded_rectangle
+*/
+var theme_soft = {
+	ancestor_shape : "rounded_rectangle",
+	child_shape : "rounded_rectangle",
+	line_shape : "straight",
+
+	//decoration
+    background_color : "#8e8e8e",
+    line_color : "#667b99",
+    focus_border : "none",
+    common_border : "none",
+    focus_element_color : "#c9ced1",
+
+	//ancestor_color
+	anc_element_color : "#8e8e8e",
+	anc_text_color : "#e75d66",
+
+    //child_color
+	ch_element_color : "#8e8e8e",
+	ch_text_color : "#99ca6a",
+
+    //grandchild_color 
+    gra_element_color : "#8e8e8e"；
+    gra_text_color : "#000";
+
+	//element_margin
+    father_child_margin : 20,
+	brother_margin : 15,
+
+	//ancestor_size
+	anc_element_width : 80,
+	anc_element_height : 35,
+
+	//child_size
+	ch_element_width : 60,
+	ch_element_height : 27,
+
+	//else
+	background_img : "none"
+}
+
