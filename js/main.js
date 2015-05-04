29300cb62e22d3b0072a1fcd4653e8ba6bdb1305
qqstar <624447192@qq.com>
2015-05-04 13:32:29 +0800

    Optimize the change patterns function.

--- js/main.js
+++ js/main.js
@@ -6,23 +6,45 @@ $(document).ready(function() {
     var cxt = canvas.getContext('2d');
     $('.pattern').click(function(){
         var this_id = $(this).attr("id");
-        if(this_id == "right"){
-            SCUTMind.currPattern = SCUTMind.patterns.default;
+        if((this_id=="right" && SCUTMind.currPattern==SCUTMind.patterns.default)||
+           (this_id=="tree" && SCUTMind.currPattern==SCUTMind.patterns.tree)||
+           (this_id=="organization" && SCUTMind.currPattern==SCUTMind.patterns.organize)){
+            return false;
         }
-        else if(this_id == "tree"){
-            SCUTMind.currPattern = SCUTMind.patterns.tree;
+        else{
+            if(((this_id=="tree"||this_id=="organization")&&SCUTMind.currPattern==SCUTMind.patterns.default)||
+               (this_id=="right"&&(SCUTMind.currPattern==SCUTMind.patterns.tree||SCUTMind.currPattern==SCUTMind.patterns.organize))){
+                if(this_id == "right") {
+                    SCUTMind.currPattern = SCUTMind.patterns.default;
+                }
+                else if(this_id == "tree") {
+                    SCUTMind.currPattern = SCUTMind.patterns.tree;
+                }
+                else if(this_id == "organization") {
+                    SCUTMind.currPattern = SCUTMind.patterns.organize;
+                }
+                SCUTMind.updateAllArea(SCUTMind.rootNode);
+                for(var i=0; i<10; i++) {
+                    if (SCUTMind.currPattern == SCUTMind.patterns.default)
+                        SCUTMind.updatePosition(SCUTMind.rootNode, SCUTMind.rootNode.position[1] - SCUTMind.rootNode.area[1] / 2);
+                    else
+                        SCUTMind.updatePosition(SCUTMind.rootNode, SCUTMind.rootNode.position[0] - SCUTMind.rootNode.area[0] / 2);
+                    SCUTMind.updateScope(SCUTMind.rootNode);
+                }
+            }
+            else{
+                if(this_id == "right") {
+                    SCUTMind.currPattern = SCUTMind.patterns.default;
+                }
+                else if(this_id == "tree") {
+                    SCUTMind.currPattern = SCUTMind.patterns.tree;
+                }
+                else if(this_id == "organization") {
+                    SCUTMind.currPattern = SCUTMind.patterns.organize;
+                }
+            }
         }
-        else if(this_id == "organization"){
-            SCUTMind.currPattern = SCUTMind.patterns.organize;
-        }
-        SCUTMind.updateAllArea(SCUTMind.rootNode);
-        for(var i=0; i<10; i++){
-            if(SCUTMind.currPattern == SCUTMind.patterns.default)
-                SCUTMind.updatePosition(SCUTMind.rootNode, SCUTMind.rootNode.position[1] - SCUTMind.rootNode.area[1]/2);
-            else
-                SCUTMind.updatePosition(SCUTMind.rootNode, SCUTMind.rootNode.position[0] - SCUTMind.rootNode.area[0]/2);
-            SCUTMind.updateScope(SCUTMind.rootNode);
-        }
+
         cxt.clearRect(0,0,canvas.width,canvas.height);
         SCUTMind.draws(cxt, SCUTMind.rootNode);
     });
@@ -35,16 +57,7 @@ $(document).ready(function() {
             alert("You can't add a sibling element of the rootNode");
             return false;
         }
-        var position = [];
-        if(SCUTMind.currPattern == SCUTMind.patterns.default){
-            position[0] = SCUTMind.currNode.position[0];
-            position[1] = SCUTMind.currNode.position[1] + SCUTMind.currNode.area[1]/2 + SCUTMind.currTheme.brother_margin + SCUTMind.currTheme.ch_element_height/2;
-        }
-        else{
-            position[0] = SCUTMind.currNode.position[0] + SCUTMind.currNode.area[0]/2 + SCUTMind.currTheme.brother_margin + SCUTMind.currTheme.ch_element_width/2;
-            position[1] = SCUTMind.currNode.position[1];
-        }
-		var newNode = new MindNode("child",SCUTMind.currNode.parent, position);
+		var newNode = new MindNode("child",SCUTMind.currNode.parent, [0,0]);
         SCUTMind.currNode.parent.addChild(newNode);
         SCUTMind.updateArea(newNode);
 		if(SCUTMind.currPattern == SCUTMind.patterns.default)
@@ -57,26 +70,7 @@ $(document).ready(function() {
 		return false;
 	});
 	$('.child-btn').click(function(event) {
-        var position = [];
-        if(SCUTMind.currPattern == SCUTMind.patterns.default){
-            position[0] = SCUTMind.currNode.scope[2] + SCUTMind.currTheme.father_child_margin + SCUTMind.currTheme.ch_element_width/2;
-            if(SCUTMind.currNode.children.length == 0){
-                position[1] = SCUTMind.currNode.position[1];
-            }
-            else{
-                position[1] = SCUTMind.currNode.children[SCUTMind.currNode.children.length-1].position[1] + SCUTMind.currNode.children[SCUTMind.currNode.children.length-1].area[1]/2 + SCUTMind.currTheme.brother_margin + SCUTMind.currTheme.ch_element_height/2;
-            }
-        }
-        else{
-            position[1] = SCUTMind.currNode.scope[3] + SCUTMind.currTheme.father_child_margin + SCUTMind.currTheme.ch_element_height/2;
-            if(SCUTMind.currNode.children.length == 0){
-                position[0] = SCUTMind.currNode.position[0];
-            }
-            else{
-                position[0] = SCUTMind.currNode.children[SCUTMind.currNode.children.length-1].position[0] + SCUTMind.currNode.children[SCUTMind.currNode.children.length-1].area[0]/2 + SCUTMind.currTheme.brother_margin + SCUTMind.currTheme.ch_element_width/2;
-            }
-        }
-		var newNode = new MindNode("child",SCUTMind.currNode,position);
+		var newNode = new MindNode("child",SCUTMind.currNode,[0,0]);
 		SCUTMind.currNode.addChild(newNode);
         newNode.scope = SCUTMind.initNodeScope("child",newNode.position,newNode.text);
 		SCUTMind.updateArea(newNode);
@@ -132,7 +126,6 @@ $(document).ready(function() {
         return false;
     });
     $('.swiper-slide a').click(function(){
-        //alert($(this).attr("id"));
         var this_id = $(this).attr("id");
         if(this_id == "gray")
             SCUTMind.currTheme = SCUTMind.themes.default;
